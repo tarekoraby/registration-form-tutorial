168e983faef609918fbe00fd781f5bd75719e67c
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-03-04 14:49:19 +0200

    Upgrade Vaadin to 23.0.0

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -3,7 +3,6 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.vaadin.artur.helpers.LaunchUtil;
 import com.vaadin.flow.component.dependency.NpmPackage;
 
 /**
@@ -15,7 +14,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+        SpringApplication.run(Application.class, args);
     }
 
 }
