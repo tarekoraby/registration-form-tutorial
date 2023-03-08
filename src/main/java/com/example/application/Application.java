8794faa02552859eba45e01f7195bdc3fe3f9d6b
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-03-08 09:44:31 +0200

    Update to Vaadin 24.0.0

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,17 +1,16 @@
 package com.example.application;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import com.vaadin.flow.component.dependency.NpmPackage;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
-@NpmPackage(value = "line-awesome", version = "1.3.0")
-public class Application extends SpringBootServletInitializer {
+@Theme(value = "myapp")
+public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/example/application/UserDetails.java
+++ src/main/java/com/example/application/UserDetails.java
@@ -1,8 +1,8 @@
 package com.example.application;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
 
 /**
